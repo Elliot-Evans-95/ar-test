d860f2d93a5e9b90da780e2209e99c0f4229c543
Elliot Evans <ell15evans.nuls@googlemail.com>
2018-07-16 14:00:17 +0100

    Revert "changed the object to a box"
    This reverts commit fe17758a7fcd53e049923eedb29b6cea80b59000.

--- src/Entities/BasicTorus.js
+++ src/Entities/BasicTorus.js
@@ -7,19 +7,14 @@ export default class BasicTorus extends baseEntity {
         super(name);
         this.mesh = this.getTorusMesh(scene, initialPosition);
         this.mesh.userData.parent = this;
-        this.boxParameters = {
-            width: 10,
-            height: 10,
-            depth: 2,
-            widthSegments: 5,
-            heightSegments: 5,
-            depthSegments: 5
-        }
+        this.radius = 0.4;
+        this.tube = 0.1;
+        this.radialSegments = 20;
+        this.tubularSegments = 30;
     }
 
     getTorusMesh(scene, initialPosition) {
-        const geometry = new THREE.BoxGeometry(this.boxParameters.width, this.boxParameters.height, this.boxParameters.depth, this.boxParameters.widthSegments,
-            this.boxParameters.heightSegments, this.boxParameters.depthSegments);
+        const geometry = new THREE.TorusGeometry(this.radius, this.tube, this.radialSegments, this.tubularSegments);
         const material = new THREE.MeshPhongMaterial({ color: 0x0B5394 });
         const torus = new THREE.Mesh(geometry, material);
         torus.position.copy(initialPosition);
