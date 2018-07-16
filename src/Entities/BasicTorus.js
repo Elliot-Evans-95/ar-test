04a4fe6874f52c82b7fd958e2239de89c2eb2bc5
Elliot Evans <ell15evans.nuls@googlemail.com>
2018-07-16 13:45:56 +0100

    changed the color of the torus

--- src/Entities/BasicTorus.js
+++ src/Entities/BasicTorus.js
@@ -2,14 +2,20 @@ import * as THREE from 'three';
 import baseEntity from './BaseEntity';
 
 export default class BasicTorus extends baseEntity {
+
   constructor(name, scene, initialPosition) {
     super(name);
     this.mesh = this.getTorusMesh(scene, initialPosition);
     this.mesh.userData.parent = this;
+    this.radius = 0.4;
+    this.tube = 0.1;
+    this.radialSegments = 20;
+    this.tubularSegments = 30;
   }
+
   getTorusMesh(scene, initialPosition) {
-    const geometry = new THREE.TorusGeometry(0.4, 0.1, 20, 30);
-    const material = new THREE.MeshPhongMaterial({ color: 0xff5643 });
+    const geometry = new THREE.TorusGeometry(this.radius, this.tube, this.radialSegments, this.tubularSegments);
+    const material = new THREE.MeshPhongMaterial({ color: 0x0B5394 });
     const torus = new THREE.Mesh(geometry, material);
     torus.position.copy(initialPosition);
     scene.add(torus);
--- src/Managers/Renderer.js
+++ src/Managers/Renderer.js
@@ -1,39 +1,45 @@
 import * as THREE from 'three';
-import { ARUtils, ARView } from 'three.ar.js';
+import {ARUtils, ARView, ARDebug} from 'three.ar.js';
 
 class Renderer {
-  constructor() {
-    this.renderer = new THREE.WebGLRenderer({ alpha: true });
-  }
+    constructor() {
+        this.renderer = new THREE.WebGLRenderer({ alpha: true });
+    }
 
-  initRenderer() {
-    return ARUtils.getARDisplay().then((display) => {
-      if (display) {
-        this.vrDisplay = display;
-        this.setupRenderer();
-        return true;
-      }
-      ARUtils.displayUnsupportedMessage();
-      return false;
-    });
-  }
+    initRenderer() {
+        return ARUtils.getARDisplay().then((display) => {
+            if (display) {
+                this.vrDisplay = display;
+                Renderer.enableDebug();
+                this.setupRenderer();
+                return true;
+            }
+            ARUtils.displayUnsupportedMessage();
+            return false;
+        });
+    }
 
-  setupRenderer() {
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.autoClear = false;
-    this.canvas = this.renderer.domElement;
-    document.body.appendChild(this.canvas);
-    this.arView = new ARView(this.vrDisplay, this.renderer);
-  }
+    setupRenderer() {
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.autoClear = false;
+        this.canvas = this.renderer.domElement;
+        document.body.appendChild(this.canvas);
+        this.arView = new ARView(this.vrDisplay, this.renderer);
+    }
 
-  update(scene, camera, updateCallback) {
-    this.renderer.clearColor();
-    this.arView.render();
-    this.renderer.clearDepth();
-    this.renderer.render(scene, camera);
-    this.vrDisplay.requestAnimationFrame(updateCallback);
-  }
+    update(scene, camera, updateCallback) {
+        this.renderer.clearColor();
+        this.arView.render();
+        this.renderer.clearDepth();
+        this.renderer.render(scene, camera);
+        this.vrDisplay.requestAnimationFrame(updateCallback);
+    }
+
+    static enableDebug() {
+        const arDebug = new ARDebug(vrDisplay);
+        document.body.appendChild(arDebug.getElement());
+    }
 }
 
 export default new Renderer();
